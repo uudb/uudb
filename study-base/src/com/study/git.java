1253af708fd6716409cce6829b25e5e2021b218b
a <a@LAPTOP-MDPLJNFU>
2017-12-27 16:44:42 +0800

    test

--- study-base/src/com/study/git.java
+++ study-base/src/com/study/git.java
@@ -2,14 +2,17 @@
  * Project Name:study-base 
  * File Name:git.java 
  * Package Name:com.study 
- * Date:2016-8-12下午3:52:55 
+ * Date:2016-8-12涓嬪崍3:52:55 
  * Copyright (c) 2016, zym All Rights Reserved. 
- * 药药好（杭州）网络科技有限公司
+ * 鑽嵂濂斤紙鏉窞锛夌綉缁滅鎶�鏈夐檺鍏徃
 */  
   
 package com.study;  
 
 public class git {
 
+	public void test(){
+		System.out.println(0);
+	}
 }
   
