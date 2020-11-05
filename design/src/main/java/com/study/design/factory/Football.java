fffe524cf9fd0dc32104433e73c680b1480f8dd8
zhengyamin <zhengyamin@188yd.com>
2020-11-05 17:57:26 +0800

    cece

---
+++ design/src/main/java/com/study/design/factory/Football.java
@@ -0,0 +1,12 @@
+package com.study.design.factory;
+
+/**
+ * Project Name: uudb
+ * Package Name: com.study.design.factory
+ * date:  2020/11/5-17:54
+ * Copyright (c) 2020, zhengyamin All Rights Reserved.
+ * 北京亿点汇通网络科技有限公司
+ * Description:(这里用一句话描述这个类的作用)
+ */
+public class Football implements Sports{
+}
